b46077fe39784c5debb86351770f593fdb61e87c
9prashant-1223 <prashantverma1223@gmail.com>
2018-02-05 00:04:44 +0530

    basic structure

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine','ejs');
+
+app.use(express.static('./public'));
+
+app.listen(3000);
+console.log('you r listening to port 3000');
