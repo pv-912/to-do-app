09e1c7aa0318df469dfa4bda8f049a03bb5f82d0
9prashant-1223 <prashantverma1223@gmail.com>
2018-02-05 00:25:42 +0530

    views and controllers

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var toDoControllers = require('./controllers/todoControllers')
 
 var app = express();
 
 app.set('view engine','ejs');
 
 app.use(express.static('./public'));
+
+toDoControllers(app);
 
 app.listen(3000);
 console.log('you r listening to port 3000');
